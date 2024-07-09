7f41236378719256911c7ca0f0aaeec53e58ee16
Innocent <iingabir@andrew.cmu.edu>
2024-07-09 16:40:16 +0200

    Task1-KAN-4 using ES6 modules in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,48 @@
-import { resolve as _resolve, join, dirname } from 'path';
+import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export const entry = './src/index.tsx';
-export const devtool = 'inline-source-map';
-export const module = {
-  rules: [
-    {
-      test: /\.tsx?$/,
-      use: 'ts-loader',
-      exclude: /node_modules/,
+const config = {
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(path.dirname(new URL(import.meta.url).pathname), 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(path.dirname(new URL(import.meta.url).pathname), 'dist'),
     },
-    {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
-    },
-    {
-      test: /\.js$/,
-      enforce: 'pre',
-      use: ['source-map-loader'],
-    },
+    port: 3000,
+    hot: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
   ],
 };
-export const resolve = {
-  extensions: ['.tsx', '.ts', '.js'],
-};
-export const output = {
-  filename: 'bundle.js',
-  path: _resolve(__dirname, 'dist'),
-};
-export const devServer = {
-  static: {
-    directory: join(__dirname, 'dist'),
-  },
-  port: 3000,
-  hot: true,
-};
-export const plugins = [
-  new CleanWebpackPlugin(),
-  new HtmlWebpackPlugin({
-    template: './src/index.html',
-  }),
-];
+
+export default config;
