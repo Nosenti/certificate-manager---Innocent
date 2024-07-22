f93495dd2b897f0bf9388650c91303f131035317
Innocent <iingabir@andrew.cmu.edu>
2024-07-22 09:19:10 +0200

    task3-KAN-34 rebase 4/8

--- src/components/example/Example.tsx
+++ src/components/example/Example.tsx
@@ -2,18 +2,17 @@ import './Example.css';
 import certificates_ from '../../data/certificates-table';
 import { useEffect, useState } from 'react';
 import Table from '../table/Table';
+import Button from '../button/Button';
+import FormPage from '../certificate-form/CertificateForm';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import {CertificateProvider, useCertificates} from '../context/CertificateContext';
 
-/**
- * Example - Placeholder for a component to be used
- *
- */
-
-interface Certificate {
-  supplier: string;
-  certificateType: string;
-  validFrom: string;
-  validTo: string;
-}
+// interface Certificate {
+//   supplier: string;
+//   certificateType: string;
+//   validFrom: string;
+//   validTo: string;
+// }
 
 const columns = [
   { Header: 'Supplier', accessor: 'supplier' },
@@ -22,24 +21,40 @@ const columns = [
   { Header: 'Valid to', accessor: 'validTo' },
 ];
 
-function Example(): JSX.Element {
-  const [certificates, setCertificates] = useState<Certificate[]>([]);
+const CertificatesTable: React.FC = () => {
+  const { certificates } = useCertificates();
 
-  useEffect(() => {
-    setCertificates(certificates_);
-  }, []);
+  return <Table columns={columns} data={certificates} />;
+};
+
+/**
+ * Example - content wrapper for the certificates table
+ * Description - Component which has the certificates table
+ * and create certificate button
+ * @returns JSX Element
+ */
+
+const Example: React.FC = () => {
+  // const [certificates, setCertificates] = useState<Certificate[]>([]);
+
+  // useEffect(() => {
+  //   setCertificates(certificates_);
+  // }, []);
+  
   return (
+   
     <div className="ex">
       <h1>Example 1</h1>
       <Button variation='contained' size='medium'>
          <Link to="/certificates/new" style={{ color: 'inherit', textDecoration: 'none' }}>Add New Certificate</Link>
       </Button>
      
-      <Table columns={columns} data={certificates} />
+      <CertificatesTable/>
       <Routes>
         <Route path="certificates/new" element={<FormPage />} />
       </Routes>
-    </div>
+      </div>
+     
   );
 };
 
